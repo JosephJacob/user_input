1f26fc81eaeab2c264f85ffb8abf3c027f00b1ff
JosephJacob <josephjacob313@gmail.com>
2024-02-21 17:38:31 +0300

    added what the print out message would be

--- user_input.py
+++ user_input.py
@@ -5,3 +5,6 @@ location = input ("What is your location ? ")
 print ("Your name is " + name )
 print ("Your age is " + age )
 print ("Your location is " + location )
+
+message = "Your name is {} and your age is {} years old in {}". format(name, age, location)
+print(message)
