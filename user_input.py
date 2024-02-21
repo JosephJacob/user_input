699d0423fbe32170f8ee31dda2f400d658610bd9
JosephJacob <josephjacob313@gmail.com>
2024-02-21 17:34:22 +0300

    added what the output show

--- user_input.py
+++ user_input.py
@@ -1,3 +1,7 @@
 name = input ("What is your name ? ")
 age = input ("What is your age ? ")
 location = input ("What is your location ? ")
+
+print ("Your name is " + name )
+print ("Your age is " + age )
+print ("Your location is " + location )
